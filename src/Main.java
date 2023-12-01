bd73cd72df5def10dc23decf231f5a99ebf51e7b
Kawã <kawabazante@gmail.com>
2023-11-30 21:35:19 -0300

    add try catch at the loop

--- src/Main.java
+++ src/Main.java
@@ -16,38 +16,43 @@ public class Main {
         Scanner leia = new Scanner(System.in);
         int opc = 0;
         while (opc != 4){
-            System.out.println();
-            System.out.println("## ÁRVORE BINÁRIA ##");
-            System.out.println("1 - Inserir");
-            System.out.println("2 - Remover");
-            System.out.println("3 - Listar");
-            System.out.println("4 - Sair");
-            System.out.println();
-            System.out.print("Informe a opção desejada: ");
-            opc = leia.nextInt();
-            switch (opc) {
-                case 1:
-                    System.out.print("Informe o valor a ser inserido: ");
-                    int valorInserir = leia.nextInt();
-                    arvore.adicionar(valorInserir);
-                    System.out.println("Valor " + valorInserir + " inserido na árvore.");
-                    break;
-                case 2:
-                    System.out.print("Informe o valor a ser removido: ");
-                    int valorRemover = leia.nextInt();
-                    arvore.remover(valorRemover);
-                    System.out.print("Valor " + valorRemover + " removido da árvore com secesso!");
-                    break;
-                case 3:
-                    System.out.print("Listando valores da árvore: ");
-                    arvore.listaremordem();
-                    break;
-                case 4:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção invalida!");
-                    break;
+            try {
+                System.out.println();
+                System.out.println("## ÁRVORE BINÁRIA ##");
+                System.out.println("1 - Inserir");
+                System.out.println("2 - Remover");
+                System.out.println("3 - Listar");
+                System.out.println("4 - Sair");
+                System.out.println();
+                System.out.print("Informe a opção desejada: ");
+                opc = leia.nextInt();
+                switch (opc) {
+                    case 1:
+                        System.out.print("Informe o valor a ser inserido: ");
+                        int valorInserir = leia.nextInt();
+                        arvore.adicionar(valorInserir);
+                        System.out.println("Valor " + valorInserir + " inserido na árvore.");
+                        break;
+                    case 2:
+                        System.out.print("Informe o valor a ser removido: ");
+                        int valorRemover = leia.nextInt();
+                        arvore.remover(valorRemover);
+                        System.out.print("Valor " + valorRemover + " removido da árvore com sucesso!");
+                        break;
+                    case 3:
+                        System.out.print("Listando valores da árvore: ");
+                        arvore.listaremordem();
+                        break;
+                    case 4:
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção invalida!");
+                        break;
+                }
+            }catch (java.util.InputMismatchException e) {
+                System.out.println("Eei! Insira um número inteiro que seja válido");
+                leia.next(); // isso faz limpar o buffer do scanner e evita um loop infinito bjs estrelados da tulla luana
             }
         }
     }
